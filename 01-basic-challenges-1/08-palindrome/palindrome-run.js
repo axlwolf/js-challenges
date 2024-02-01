569d93fd8340606d3a3026551aeb9398e720d6ec
Francisco Axel Rojas Lanuza <axlwolf@gmail.com>
2024-02-01 12:57:38 -0600

    8. Basic Challenges: Exercice 8 completed & problem 7 test file name fixed

--- 01-basic-challenges-1/08-palindrome/palindrome-run.js
+++ 01-basic-challenges-1/08-palindrome/palindrome-run.js
@@ -1,6 +1,9 @@
-const isPalindrome = require('./palindrome');
+import { isPalindrome } from "./palindrome.js";
 
-const result1 = isPalindrome('racecar');
-const result2 = isPalindrome('racecars');
+const result1 = isPalindrome("racecar");
+const result2 = isPalindrome("racecars");
+const result3 = isPalindrome("A man, a plan, a canal, Panama");
 
-console.log(result1, result2);
+console.log({ result1, result2 });
+
+console.log({ result3 });
