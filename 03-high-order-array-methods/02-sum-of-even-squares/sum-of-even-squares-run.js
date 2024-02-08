ae05fff1b607589d9861f868aaec54a2f559e769
GDL-i5143 <user@GDL-i5143s-MacBook-Pro.local>
2024-02-07 19:27:05 -0600

    20. High order array methods 3 : 02. Sum of even squares exercise completed

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
@@ -1,4 +1,4 @@
-const sumOfEvenSquares = require('./sum-of-even-squares');
+import sumOfEvenSquares from "./sum-of-even-squares.js";
 
 const result = sumOfEvenSquares([1, 2, 3, 4, 5, 6]);
 
--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,21 @@
-function sumOfEvenSquares() {}
+// const sumOfEvenSquares = (numbers) => {
+//   const sum = numbers
+//     .filter((number) => number % 2 === 0)
+//     .reduce((sum, number) => (sum += number * number), 1);
+//   return sum - 1;
+// };
 
-module.exports = sumOfEvenSquares;
+const sumOfEvenSquares = (numbers) => {
+  // Filter out the even numbers from the input array.
+  const evenSquares = numbers
+    .filter((num) => num % 2 === 0)
+    // Square each even number.
+    .map((num) => num ** 2)
+    // Accumulate the squared numbers to compute the sum.
+    .reduce((sum, square) => sum + square, 0);
+
+  // Return the sum of squares of even numbers.
+  return evenSquares;
+};
+
+export default sumOfEvenSquares;
