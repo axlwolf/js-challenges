77a312046e680f0310ef4be48b05d107523e72ea
Axel Lanuza <axlwolf@gmail.com>
2024-02-07 19:29:27 -0600

    20. High order array methods 3 : 02. Sum of even squares exercise: added an extra cased

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
@@ -1,5 +1,7 @@
 import sumOfEvenSquares from "./sum-of-even-squares.js";
 
 const result = sumOfEvenSquares([1, 2, 3, 4, 5, 6]);
+const result2 = sumOfEvenSquares([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 console.log(result);
+console.log(result2);
