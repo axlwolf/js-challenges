9b5d80756fb4f165c8cae9ef99b29c140b282a5d
Axel Lanuza <axlwolf@gmail.com>
2024-02-07 19:55:32 -0600

    20. High order array methods 3 : 0.3 Calculate total dales with tax exrcise completed

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,23 @@
-function calculateTotalSalesWithTax() {}
+// const calculateTotalSalesWithTax = (products, taxRate) => {
+//   const total = products
+//     .map((product) => product.price * product.quantity)
+//     .reduce((total, price) => total + price, 0);
+//   const totalWithtaxRate = total + total * (taxRate / 100);
+//   return totalWithtaxRate;
+// };
 
-module.exports = calculateTotalSalesWithTax;
+const calculateTotalSalesWithTax = (products, taxRate) => {
+  //  Calculate total sales amount using the product price and quantity using the reduce method
+  const totalSales = products.reduce(
+    (sum, product) => sum + product.price * product.quantity,
+    0
+  );
+  // Calculate the tax amount using the total sales amount and the tax rate
+  const taxAmount = (totalSales * taxRate) / 100;
+  // Calculate the total sales amount with tax
+  const totalSalesWithTax = totalSales + taxAmount;
+  // Return the total sales amount with tax rounded to 2 decimal places
+  return parseFloat(totalSalesWithTax.toFixed(2));
+};
+
+export default calculateTotalSalesWithTax;
