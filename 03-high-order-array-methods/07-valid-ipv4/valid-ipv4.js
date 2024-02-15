730ff24128c7fb2792906126d4f21c1d2d3db7d9
Francisco Axel Rojas Lanuza <axlwolf@gmail.com>
2024-02-14 18:57:55 -0600

    24. High order array methods 3 : 7 Valid ipv4 exrcise completed

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,26 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (input) => {
+  // Split the input string into an array of octets.
+  const octets = input.split(".");
+  // If the input string does not contain exactly 4 octets, return false.
+  if (octets.length !== 4) {
+    return false;
+  }
+  // Otherwise, return true if every octet is a number between 0 and 255.
+  return octets.every((octet) => {
+    const num = parseInt(octet); // Convert the octet to a number.
+    return num >= 0 && num <= 255 && octet === num.toString(); // Return true if the octet is between 0 and 255 and the octet is equal to the number converted back to a string.
+  });
+};
 
-module.exports = isValidIPv4;
+// (str) => {
+//   const ipV4 = str.split(".");
+//   if (ipV4.length !== 4) return false;
+
+//   return ipV4.every((segment) => {
+//     const num = parseInt(segment); // Convert the segment to a number.
+//     return num >= 0 && num <= 255 && segment === num.toString(); // Return true if the octet is between 0 and 255 and the octet is equal to the number converted back to a string.
+//   });
+//   //  return ipV4;
+// };
+
+export default isValidIPv4;
